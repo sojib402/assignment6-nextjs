062bf8875e155b4c17831a10c9b1861e76304e28
sojib402 <sojib241-15-402@diu.edu.bd>
2026-09-25 22:57:02 +0600

    myplan r logic code complete

--- app/listedFits/page.tsx
+++ app/listedFits/page.tsx
@@ -1,14 +1,62 @@
-'use client'
+'use client';
+
 import { FitsContext } from '@/app/context/FitsContext';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 const ListedFits = () => {
-    const {addPlans,addSave}=useContext(FitsContext)
-    console.log(addPlans,addSave,'from listed fit')
+    const { addPlans, addSave } = useContext(FitsContext);
+    const [showSave, setShowSave] = useState(false);
+    const fits = showSave ? addSave : addPlans;
+    const totalDuration = fits.reduce((total, fit) => total + fit.duration, 0)
+    const totalCalories = fits.reduce((total, fit) => total + fit.caloriesBurned, 0)
+
+    console.log(addPlans, addSave, 'from listed fit');
+
     return (
         <div>
-            Listed Fits
-            {addPlans.length}
+            <div className="container mx-auto">
+                <h1 className="text-3xl text-white my-3">MY PLAN</h1>
+
+                <p className="text-white mb-5">
+                    Cap of five lifts for today. Finish them, then load more.
+                </p>
+            </div>
+
+            <div className='flex justify-between'>
+                <div>
+                    <h2>Exercises</h2>
+                    <span className="text-[#C2F800] text-4xl">
+                        {fits.length}
+                    </span>
+                </div>
+
+                <div>
+                    <h2>Minutes</h2>
+                    <span className="text-[#C2F800] text-4xl">
+                        {totalDuration}
+                    </span>
+                </div>
+
+                <div>
+                    <h2>Calories</h2>
+                    <span>{totalCalories}</span>
+                </div>
+            </div>
+            <div className="flex mt-6">
+                <button
+                    onClick={() => setShowSave(false)}
+                    className={!showSave ? 'bg-[#252830] text-white px-5 py-2' : 'text-gray-400 px-5 py-2'}
+                >
+                    Today's Plan
+                </button>
+
+                <button
+                    onClick={() => setShowSave(true)}
+                    className={showSave ? 'bg-[#252830] text-white px-5 py-2' : 'text-gray-400 px-5 py-2'}
+                >
+                    Saved
+                </button>
+            </div>
         </div>
     );
 };
