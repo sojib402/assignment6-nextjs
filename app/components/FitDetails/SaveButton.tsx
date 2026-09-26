548ecc7f905df4a20bcf49af075e7a8115da84bd
sojib402 <sojib241-15-402@diu.edu.bd>
2026-09-26 17:17:26 +0600

    ADDReadme

--- app/components/FitDetails/SaveButton.tsx
+++ app/components/FitDetails/SaveButton.tsx
@@ -4,26 +4,26 @@ import { IFit } from '@/app/types/books.type';
 import React, { useContext } from 'react';
 import { toast } from 'react-toastify';
 
-const SaveButton = ({fit}:{fit:IFit}) => {
-    const {addSave,setAddSave}:{
-        addSave:IFit[];
-        setAddSave:React.Dispatch<React.SetStateAction<IFit[]>>;
-    }=useContext(FitsContext)
+const SaveButton = ({ fit }: { fit: IFit }) => {
+    const { addSave, setAddSave }: {
+        addSave: IFit[];
+        setAddSave: React.Dispatch<React.SetStateAction<IFit[]>>;
+    } = useContext(FitsContext)
 
-    const handleSave=()=>{
-        const duplicate=addSave.some((item)=>item.id===fit.id)
-                if(duplicate){
-                    toast.error(`'${fit.name}' already exits`)
-                    return;
-                }
-        setAddSave([...addSave,fit])
+    const handleSave = () => {
+        const duplicate = addSave.some((item) => item.id === fit.id)
+        if (duplicate) {
+            toast.error(`'${fit.name}' already exits`)
+            return;
+        }
+        setAddSave([...addSave, fit])
         toast.success(`you have trigered Saved ${fit.name}`)
     }
     return (
         <div>
-            <button onClick={()=>handleSave()} className='px-3 py-2 text-white border border-white rounded-2xl text-sm sm:text-base whitespace-nowrap'>
-                            ♧ Save for later
-                        </button>
+            <button onClick={() => handleSave()} className='px-3 py-2 text-white border border-white rounded-2xl text-sm sm:text-base whitespace-nowrap'>
+                ♧ Save for later
+            </button>
         </div>
     );
 };
